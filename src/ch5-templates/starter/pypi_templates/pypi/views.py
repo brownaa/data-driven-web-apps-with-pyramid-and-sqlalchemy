f05a751bcf627f031ff6a92519887092b6916636
Brown <brownaa@mac.com>
2019-01-21 08:00:29 -0600

    Wrapped up chapter 6 of the course with a working website and CMS functionality added

--- src/ch5-templates/starter/pypi_templates/pypi/views.py
+++ src/ch5-templates/starter/pypi_templates/pypi/views.py
@@ -1,6 +1,18 @@
 from pyramid.view import view_config
 
+def get_test_packages():
+    return [
+        {'name': 'requests', 'version': '1.2.3'},
+        {'name': 'sqlalchemy', 'version': '2.0.0'},
+        {'name': "pyramid", 'version': '1.9.1'}
+    ]
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'Python Package Index'}
+@view_config(route_name='home', renderer='templates/home_index.pt')
+def home_index(_):
+    return {
+        'packages': get_test_packages()
+    }
+
+@view_config(route_name='about', renderer='templates/about.pt')
+def about(_):
+    return {}
